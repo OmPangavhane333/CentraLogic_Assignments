42edda9bd86646d02f3579aefb131acf4fc94e54
OmPangavhane333 <ompangavhane789@gmail.com>
2023-11-14 22:13:50 +0530

    Working With Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// Code to understand the concept of Enums
+
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday, 
+    Friday,
+    Saturday
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("\n____________________________");
+        Console.WriteLine("____________________________\n");
+        Console.WriteLine("Working With Enums in C#\n");
+
+        foreach (Days day in Enum.GetValues(typeof(Days)))
+        {
+            Console.WriteLine(day);
+        }
+
+    }
+}
