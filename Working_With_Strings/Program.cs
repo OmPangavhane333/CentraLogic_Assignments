f3a427192eb6553e1a754d27435b447740fdeab2
OmPangavhane333 <ompangavhane789@gmail.com>
2023-11-14 22:32:54 +0530

    Working With Strings In C#

---
+++ Working_With_Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿// Code to Working with Strings
+
+Console.WriteLine("\n__________________________________________");
+Console.WriteLine("__________________________________________\n");
+Console.WriteLine("******** Working With Strings *******\n");
+
+String str1 = "Om Pangavhane";
+Console.WriteLine("\nString is:- " + str1);
+// String Concatenation
+String a = "Hii ";
+String b = "Nana";
+Console.WriteLine("The Concatenated string is as follows \n" + a + b);
+
+/*
+ * There is one more concept in C# is String Interpolation.
+ * C# Provides it to embed expressions within strings using the "$" Character.
+ */
+Console.WriteLine("\n*********  String InterPolation In C#  **********");
+String name = "Om Pangavhane";
+int age = 21;
+String msg = $"My Name is {name} & I am {age} years Old.";
+
+Console.WriteLine("The above example shows the String Interpolation C#");
+
+Console.WriteLine("\n__________________________________________");
+Console.WriteLine("__________________________________________\n");
